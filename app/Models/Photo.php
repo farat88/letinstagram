82162995243e48a4c926c72a707986c48862456f
ХитрыйДжони <56193451+farat88@users.noreply.github.com>
2022-10-05 19:13:30 +0300

    Добавление моделей

---
+++ app/Models/Photo.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Photo extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $guarded = [];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class, 'post_id', 'id');
+    }
+}
